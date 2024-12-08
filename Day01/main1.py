92517b5908982c83d0f710a6520f864fc257e16e
Kobayashi Kyosuke <kyo0117dragonquest@gmail.com>
2024-12-08 12:33:21 +0900

    refacor Day01

--- Day01/main1.py
+++ Day01/main1.py
@@ -5,8 +5,8 @@ answer = 0
 
 with open(input_path) as file:
     for line in file:
-        list1.append(int(line[0:5]))
-        list2.append(int(line[8:13]))
+        list1.append(int(line.split()[0]))
+        list2.append(int(line.split()[1]))
 
 list1.sort()
 list2.sort()
--- Day01/main2.py
+++ Day01/main2.py
@@ -5,11 +5,8 @@ answer = 0
 
 with open(input_path) as file:
     for line in file:
-        list1.append(int(line[0:5]))
-        list2.append(int(line[8:13]))
-
-list1.sort()
-list2.sort()
+        list1.append(int(line.split()[0]))
+        list2.append(int(line.split()[1]))
 
 for loc_id in list1:
     answer += loc_id * list2.count(loc_id)
